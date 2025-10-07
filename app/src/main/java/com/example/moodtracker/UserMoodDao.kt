3b55c88fb53b107cfa3d436ff711f0ebf495e887
Minenhle Ncongwane <st10266839@imconnect.edu.za>
2025-10-07 17:38:31 +0200

    [Minenhle Ncongwane] Add 20 placeholder comments to UserMoodDao.kt

--- app/src/main/java/com/example/moodtracker/UserMoodDao.kt
+++ app/src/main/java/com/example/moodtracker/UserMoodDao.kt
@@ -1,3 +1,23 @@
+// TODO: Comment 1
+// TODO: Comment 2
+// TODO: Comment 3
+// TODO: Comment 4
+// TODO: Comment 5
+// TODO: Comment 6
+// TODO: Comment 7
+// TODO: Comment 8
+// TODO: Comment 9
+// TODO: Comment 10
+// TODO: Comment 11
+// TODO: Comment 12
+// TODO: Comment 13
+// TODO: Comment 14
+// TODO: Comment 15
+// TODO: Comment 16
+// TODO: Comment 17
+// TODO: Comment 18
+// TODO: Comment 19
+// TODO: Comment 20
 package com.example.moodtracker
 
 import androidx.lifecycle.LiveData
@@ -19,3 +39,4 @@ interface UserMoodDao {
     @Query("DELETE FROM user_moods WHERE userId = :userId")
     suspend fun deleteAllForUser(userId: String)
 }
+
